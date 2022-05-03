21e0396c618e5d31eb6779452e3258fe4c19b052
James Payne <james.payne1349@gmail.com>
2022-05-03 13:37:13 -0500

    cloud svg added

---
+++ src/components/ServicesContainer/ConsultingCloud.svelte
@@ -0,0 +1,38 @@
+<script>
+
+
+</script>
+
+
+
+<svg width="855px" height="575px" viewBox="0 0 855 575" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <defs>
+        <filter x="-5.1%" y="-7.5%" width="110.2%" height="115.8%" filterUnits="objectBoundingBox" id="filter-v_2qllf4d2-1">
+            <feOffset dx="2" dy="-2" in="SourceAlpha" result="shadowOffsetOuter1"></feOffset>
+            <feGaussianBlur stdDeviation="8.5" in="shadowOffsetOuter1" result="shadowBlurOuter1"></feGaussianBlur>
+            <feColorMatrix values="0 0 0 0 0.631372549   0 0 0 0 0.678431373   0 0 0 0 0.756862745  0 0 0 0.224570108 0" type="matrix" in="shadowBlurOuter1" result="shadowMatrixOuter1"></feColorMatrix>
+            <feMerge>
+                <feMergeNode in="shadowMatrixOuter1"></feMergeNode>
+                <feMergeNode in="SourceGraphic"></feMergeNode>
+            </feMerge>
+        </filter>
+        <path d="M630.677755,513 L162.322245,513 C72.8265306,513 0,440.061611 0,350.428436 C0,270.84455 56.9665306,203.903318 134.000816,190.450237 C164.749796,112.649289 239.842041,60.9440758 324.15898,60.9440758 C336.782245,60.9440758 349.567347,62.2407583 362.676122,64.8341232 C401.355102,23.5023697 455.246735,0 511.889592,0 C624.689796,0 716.451224,91.9023697 716.451224,204.875829 C716.451224,207.469194 716.451224,209.900474 716.289388,212.493839 C763.869388,241.993365 793,293.860664 793,350.428436 C793,440.061611 720.173469,513 630.677755,513 Z" id="path-v_2qllf4d2-2"></path>
+        <filter x="-0.9%" y="-1.0%" width="101.8%" height="102.7%" filterUnits="objectBoundingBox" id="filter-v_2qllf4d2-3">
+            <feOffset dx="0" dy="2" in="SourceAlpha" result="shadowOffsetOuter1"></feOffset>
+            <feGaussianBlur stdDeviation="2" in="shadowOffsetOuter1" result="shadowBlurOuter1"></feGaussianBlur>
+            <feColorMatrix values="0 0 0 0 0   0 0 0 0 0   0 0 0 0 0  0 0 0 0.5 0" type="matrix" in="shadowBlurOuter1"></feColorMatrix>
+        </filter>
+    </defs>
+    <g id="Homepage-desktop" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g id="Homepage" transform="translate(-570.000000, -2260.000000)" fill-rule="nonzero">
+            <g id="cloud-svg" filter="url(#filter-v_2qllf4d2-1)" transform="translate(601.000000, 2289.000000)">
+                <g id="Path">
+                    <use fill="black" fill-opacity="1" filter="url(#filter-v_2qllf4d2-3)" xlink:href="#path-v_2qllf4d2-2"></use>
+                    <use fill="#0A162C" xlink:href="#path-v_2qllf4d2-2"></use>
+                </g>
+            </g>
+        </g>
+    </g>
+</svg>
+
+
