3919f472c3032a0381d986d591a6a3131415b6a7
James <james.payne1349@gmail.com>
2022-05-22 10:54:59 -0500

    catch blocks added to analytics promises

--- src/initFirebase.js
+++ src/initFirebase.js
@@ -1,49 +1,54 @@
 import firebaseConfig from './env';
 import { browser, dev } from '$app/env';
 
-
 export async function initFirebase() {
-
 	console.log('Running in browser: ', browser);
 	console.log('Development Mode: ', dev);
 
 	try {
-        // our way of only running this in the browser.
+		// our way of only running this in the browser.
 		if (typeof window != undefined) {
-			
 			let { initializeApp } = await import('firebase/app');
-			let { getAnalytics , isSupported } = await import('firebase/analytics');
+			let { getAnalytics, isSupported } = await import('firebase/analytics');
 			let { getFirestore } = await import('firebase/firestore');
-			let {initializeAppCheck, ReCaptchaV3Provider } = await import('firebase/app-check');
+			let { initializeAppCheck, ReCaptchaV3Provider } = await import('firebase/app-check');
 
 			const app = initializeApp(firebaseConfig);
-            
-			let analytics = isSupported().then(()=>{
-				console.log('Analytics is supported');
-				return getAnalytics(app);
-			});
-	
+
+			let analytics = isSupported()
+				.then(() => {
+					try {
+						getAnalytics(app);
+					} catch (e) {
+						console.log('Error in getAnalytics');
+						console.error(e);
+					}
+				})
+				.catch((e) => {
+					console.log('Error in isSupported()');
+					console.error(e);
+				});
+
 			const db = getFirestore(app);
 
-			if(dev) {
+			if (dev) {
 				self.FIREBASE_APPCHECK_DEBUG_TOKEN = true;
 			}
-			
+
 			const appCheck = initializeAppCheck(app, {
 				provider: new ReCaptchaV3Provider('6LdXPukfAAAAAFJjgZDYYiMhDMLZKiirlzgp-oho'),
 
-			 	isTokenAutoRefreshEnabled: true
-			 });
+				isTokenAutoRefreshEnabled: true
+			});
 
-			return { app , db};
+			return { app, db };
 		}
 
 		// code here to run on server side
-        // not needed at this time?
-
+		// not needed at this time?
 	} catch (error) {
-		console.log('Error in Firebase Init');
+		console.error('Error in Firebase Init');
 
-        // TODO: return a trigger to display an error in the contact form.
+		// TODO: return a trigger to display an error in the contact form.
 	}
 }
