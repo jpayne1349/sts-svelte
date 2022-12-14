71d4a4619370f498b0b1fa3346f09913878b1de6
James Payne <james.payne1349@gmail.com>
2022-12-14 14:36:14 -0600

    minor updates to the way links are shown in the development page of the client portal. straight push to prod!

--- src/routes/client/portal/development/+page.svelte
+++ src/routes/client/portal/development/+page.svelte
@@ -8,23 +8,28 @@
 		<div class="information-row">
 			<p class="label">Active Links:</p>
 			<!-- loop through active links -->
-			{#each $sessionStore.service_log.active_links as link}
-				<p class="data">{link}</p>
+			{#each $sessionStore.service_log.active_links as link, index}
+				<a class="service-link" href={link}>Production/Public</a>
 			{/each}
 		</div>
 
 		<div class="information-row">
 			<p class="label">Preview Links:</p>
 			<!-- loop through active links -->
-			{#each $sessionStore.service_log.preview_links as link}
-				<p class="data">{link}</p>
+			{#each $sessionStore.service_log.preview_links as link, index}
+				<a class="service-link" href={link}>Latest Update</a>
 			{/each}
 		</div>
 
 		<div class="information-row">
 			<p class="label">Codebase/Repo:</p>
 
-			<p class="data">{$sessionStore.service_log.code_repo}</p>
+			<p class="data">
+				{#if $sessionStore.service_log.code_repo}
+					<a class="service-link" href={$sessionStore.service_log.code_repo}>Public Project Files</a
+					>
+				{/if}
+			</p>
 		</div>
 
 		<div class="information-row events">
@@ -78,7 +83,7 @@
 	}
 	.hide-scrollbar {
 		width: 100%;
-		box-sizing:content-box;
+		box-sizing: content-box;
 		overflow-x: hidden;
 	}
 	.events-container {
@@ -88,7 +93,11 @@
 		align-items: center;
 		max-height: 300px;
 		overflow-y: scroll;
-		
+	}
+	.service-link {
+		color: var(--link);
+		font-family: openSans-light;
+		text-decoration: underline;
 	}
 
 	@media only screen and (max-width: 500px) {
