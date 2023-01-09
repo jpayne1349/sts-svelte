9542b8de30ab5d7927f4702204835537a9f2bb5d
James Payne <james.payne1349@gmail.com>
2023-01-09 09:22:24 -0600

    addition of coming soon/construction animated svg to homepage

--- src/routes/components/LandingView/LandingContainer.svelte
+++ src/routes/components/LandingView/LandingContainer.svelte
@@ -1,25 +1,23 @@
 <script>
-import MainPitch from './MainPitch.svelte';
+	import MainPitch from './MainPitch.svelte';
 
-import ButtonContainer from './ButtonContainer.svelte';
-import UnderButtonPhrase from './UnderButtonPhrase.svelte';
-
-
+	import ButtonContainer from './ButtonContainer.svelte';
+	import UnderButtonPhrase from './UnderButtonPhrase.svelte';
+	import ComingSoonSvg from './ComingSoonSVG.svelte';
 </script>
 
-<div id='landing-container'>
-    <MainPitch />
-    <ButtonContainer />
-    <UnderButtonPhrase />
+<div id="landing-container">
+	<MainPitch />
 
+	<ButtonContainer />
+	<UnderButtonPhrase />
+	<ComingSoonSvg />
 </div>
 
-
 <style>
-    #landing-container {
-        height: 800px;
-        position: relative;
-        width: 100vw;
-    }
-
+	#landing-container {
+		height: 800px;
+		position: relative;
+		width: 100vw;
+	}
 </style>
